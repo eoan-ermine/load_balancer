d4182e631c4240201a1339df54eaa82f20225b7c
PatriotRossii <patriotrossii2019@mail.ru>
2022-08-20 13:33:04 +0300

    Rewrite relay in async way

--- include/relay.hpp
+++ include/relay.hpp
@@ -1,16 +1,16 @@
-template<bool isRequest, class SyncWriteStream, class SyncReadStream, class DynamicBuffer>
-void relay(SyncWriteStream& output, SyncReadStream& input, DynamicBuffer& buffer, boost::beast::error_code& ec) {
-    static_assert(boost::beast::is_sync_write_stream<SyncWriteStream>::value,
-        "SyncWriteStream requirements not met");
+template<bool isRequest, class AsyncWriteStream, class AsyncReadStream, class DynamicBuffer>
+void relay(AsyncWriteStream& output, AsyncReadStream& input, DynamicBuffer& buffer, boost::beast::error_code& ec, boost::asio::yield_context yield) {
+    static_assert(boost::beast::is_async_write_stream<AsyncWriteStream>::value,
+        "AsyncWriteStream requirements not met");
 
-    static_assert(boost::beast::is_sync_read_stream<SyncReadStream>::value,
-        "SyncReadStream requirements not met");
+    static_assert(boost::beast::is_async_read_stream<AsyncReadStream>::value,
+        "AsyncReadStream requirements not met");
 
     boost::beast::http::parser<isRequest, boost::beast::http::buffer_body> p;
-    boost::beast::http::read_header(input, buffer, p, ec);
-    if(ec) return;
+    boost::beast::http::async_read_header(input, buffer, p, yield[ec]);
+    if (ec) return;
 
     boost::beast::http::serializer<isRequest, boost::beast::http::buffer_body, boost::beast::http::fields> sr{p.get()};
-    boost::beast::http::write_header(output, sr, ec);
-    if(ec) return;
+    boost::beast::http::async_write_header(output, sr, yield[ec]);
+    if (ec) return;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/spawn.hpp>
 #include <boost/config.hpp>
+
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <thread>
+#include <vector>
 
 #include "../include/relay.hpp"
 
@@ -16,18 +20,70 @@ namespace http = beast::http;
 namespace net = boost::asio;
 using tcp = boost::asio::ip::tcp;
 
-void do_session(tcp::socket& socket, beast::tcp_stream& output) {    
+void fail(beast::error_code ec, char const* what) {
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+void do_session(beast::tcp_stream& client, beast::tcp_stream& target, net::yield_context yield) {    
     beast::error_code ec;
     beast::flat_buffer buffer;
 
-    relay<true>(output, socket, buffer, ec);
+    relay<true>(target, client, buffer, ec, yield);
     
     http::response<http::dynamic_body> res;
-    http::read(output, buffer, res, ec);
-    http::write(socket, res, ec);
+    http::async_read(target, buffer, res, yield[ec]);
+    if (ec) return fail(ec, "response read");
 
-    socket.shutdown(tcp::socket::shutdown_send, ec);
-    output.close();
+    http::async_write(client, res, yield[ec]);
+    if (ec) return fail(ec, "response write");
+
+    client.socket().shutdown(tcp::socket::shutdown_send, ec);
+    target.close();
+}
+
+void do_listen(net::io_context& ioc, tcp::endpoint endpoint, std::pair<std::string, std::string> forward, net::yield_context yield) {
+    beast::error_code ec;
+
+    tcp::acceptor acceptor{ioc};
+    acceptor.open(endpoint.protocol(), ec);
+    if (ec)
+        return fail(ec, "open");
+
+    acceptor.set_option(net::socket_base::reuse_address(true), ec);
+    if (ec)
+        return fail(ec, "set_option");
+
+    acceptor.bind(endpoint, ec);
+    if (ec)
+        return fail(ec, "bind");
+
+    acceptor.listen(net::socket_base::max_listen_connections, ec);
+    if (ec)
+        return fail(ec, "listen");
+
+    tcp::resolver resolver(ioc);
+    const auto results = resolver.async_resolve(forward.first, forward.second, yield[ec]);
+    if (ec)
+        return fail(ec, "async_resolve");
+
+    while (true) {
+        tcp::socket client_socket{ioc};
+        acceptor.async_accept(client_socket, yield[ec]);
+        if (ec)
+            fail(ec, "accept");
+
+        beast::tcp_stream target_stream{ioc};
+        target_stream.async_connect(results, yield[ec]);
+        if (ec)
+            fail(ec, "connect");
+
+        else
+            boost::asio::spawn(
+                acceptor.get_executor(), std::bind(
+                    &do_session, beast::tcp_stream(std::move(client_socket)), std::move(target_stream), std::placeholders::_1
+                )
+            );
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -35,26 +91,26 @@ int main(int argc, char* argv[]) {
         std::cout << "Usage:\n"
                   << "./a.out [listener_host] [listener_port] [target_host] [target_port]"
                   << std::endl;
-        return 1;
+        return EXIT_FAILURE;
     }
 
     const auto host = net::ip::make_address(argv[1]);
     const auto port = static_cast<unsigned short>(std::atoi(argv[2]));
     const auto target_host = argv[3], target_port = argv[4];
 
-    net::io_context ioc{1};
-    tcp::acceptor acceptor{ioc, {host, port}};
+    unsigned int threads = std::thread::hardware_concurrency();
+    net::io_context ioc{threads};
 
-    tcp::resolver resolver(ioc);
-    auto const results = resolver.resolve(target_host, target_port);
+    boost::asio::spawn(ioc, std::bind(
+        &do_listen, std::ref(ioc), tcp::endpoint{host, port}, std::make_pair(target_host, target_port), std::placeholders::_1
+    ));
 
-    while (true) {
-        tcp::socket socket{ioc};
-        beast::tcp_stream stream(ioc);
-
-        acceptor.accept(socket);
-        stream.connect(results);
-
-        std::thread(std::bind(&do_session, std::move(socket), std::move(stream))).detach();
+    std::vector<std::thread> v;
+    v.reserve(threads - 1);
+    for (auto i = threads - 1; i != 0; --i) {
+        v.emplace_back([&ioc] { ioc.run(); });
     }
+    ioc.run();
+
+    return EXIT_SUCCESS;
 }
