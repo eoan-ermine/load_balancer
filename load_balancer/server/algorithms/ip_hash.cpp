7aa17bb8825ce1022c174cf3c37e6bc2d125cbd9
PatriotRossii <patriotrossii2019@mail.ru>
2023-03-12 20:10:25 +0300

    Implement ip hash algorithm
    * Implement IPHash class
    * Add ip_hash.cpp to CMakeLists.txt

---
+++ load_balancer/server/algorithms/ip_hash.cpp
@@ -0,0 +1,22 @@
+#include <functional>
+
+#include <load_balancer/server/algorithms/ip_hash.hpp>
+
+namespace eoanermine {
+
+namespace load_balancer {
+
+IPHash::IPHash(std::vector<TargetInfo> &targets) : targets(targets) { }
+
+const TargetInfo &IPHash::getNext(const RequestInfo& request) {
+  std::size_t hash = std::hash<unsigned long>{}(
+    request.address.to_v4().to_ulong()
+  );
+  return targets[hash % targets.size()];
+};
+
+IPHash::~IPHash() {}
+
+} // namespace load_balancer
+
+} // namespace eoanermine
